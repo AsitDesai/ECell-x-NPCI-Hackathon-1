ca90e30221ac4e1b7a010eb4a663498ec702f77c
AdityaKharmaleGoat7 <adityakharmale7@gmail.com>
2025-02-01 19:31:01 +0530

    commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -87,14 +87,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  // Navigate to QR scanner screen
-  // void _navigateToQrScanner(BuildContext context) {
-  //   Navigator.push(
-  //     context,
-      
-  //     MaterialPageRoute(builder: (context) => QrScannerScreen()),
-  //   );
-  // }
 
   // Build the profile menu
   Widget _buildProfileMenu() {
