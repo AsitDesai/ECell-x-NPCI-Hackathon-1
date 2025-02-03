0c214f03c4313a60dba76444ced1bd9989c5b603
AdityaKharmaleGoat7 <adityakharmale7@gmail.com>
2025-02-03 16:25:50 +0530

    commit

--- lib/screens/add_manually.dart
+++ lib/screens/add_manually.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../database/database_helper.dart';
+// import 'package:provider/provider.dart';
+// import '../database/database_helper.dart';
 
 class AddManuallyScreen extends StatefulWidget {
   final int transactionId;
